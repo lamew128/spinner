91afef6da04c66da77c959bfc35c3806401399fd
Eugene <lamew128@gmail.com>
2022-04-11 23:49:36 -0400

    spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,11 +1,11 @@
-let i = 0;
-while(i < 3200) {
-  setTimeout(() => process.stdout.write('\\\r'), i);
-  i += 200;
-  setTimeout(() => process.stdout.write('|\r'), i);
-  i += 200;
-  setTimeout(() => process.stdout.write('/\r'), i);
-  i += 200;
-  setTimeout(() => process.stdout.write('-\r'), i);
-  i += 200;
+let j = 0;
+let bar = ["\\", "|", "/", "-"];
+for (let i = 0; i < 3200; i += 200) {
+  setTimeout(() => {
+    process.stdout.write("\r" + bar[j] + "  ");
+    j++;
+    if (j === 4) 
+      j = 0;
+  }, i);
 }
+setTimeout(() => process.stdout.write("\r"), 3400);
